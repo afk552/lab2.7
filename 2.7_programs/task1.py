446e55a5de9cce78c0276191201011dd405958fa
afk552 <ez552@mail.ru>
2022-12-05 00:00:21 +0300

    Fix tasks

--- 2.7_programs/task1.py
+++ 2.7_programs/task1.py
@@ -4,6 +4,9 @@
 if __name__ == '__main__':
     stroka = (str(input("Введите строку: "))).lower()
     vowels = {'а', 'я', 'у', 'ю', 'о', 'е', 'ё', 'э', 'и', 'ы'}
-    only_vowels = set(list(stroka))
+    expt = {'.', ' ', '!', '?', ','}
+
+    only_vowels = (set(list(stroka))).difference(expt)
+    only_vowels = only_vowels.intersection(vowels)
     print(only_vowels)
     print("Количество гласных в строке: ", len(only_vowels))
